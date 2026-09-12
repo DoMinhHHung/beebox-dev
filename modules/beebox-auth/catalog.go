2a384a890e8e24a8472fd58e4a5081e1346c27b2
DoMinhHHung <dominhhung04032003@gmail.com>
2026-09-12 22:30:35 +0700

    update: catalog

--- modules/beebox-auth/catalog.go
+++ modules/beebox-auth/catalog.go
@@ -1,5 +1,32 @@
 package beeboxauth
 
+type ChangeKind string
+
+const (
+	ChangeAdditive     ChangeKind = "ADDITIVE"
+	ChangeIncompatible ChangeKind = "INCOMPATIBLE"
+)
+
+type ModuleDefinition struct {
+	ID      string
+	Version string
+}
+
+type CapabilityDefinition struct {
+	ModuleID string
+	ID       string
+	Version  string
+}
+
+type DataFieldDefinition struct {
+	ModuleID          string
+	CapabilityID      string
+	CapabilityVersion string
+	ID                string
+	Version           string
+	ChangeKind        ChangeKind
+}
+
 const (
 	ModuleID      = "beebox-auth"
 	ModuleVersion = "v1"
@@ -15,3 +42,28 @@ const (
 	FieldPasswordHash    = "password_hash"
 	FieldPasswordHashVer = "v1"
 )
+
+func DefaultDefinitions() (
+	modules []ModuleDefinition,
+	capabilities []CapabilityDefinition,
+	dataFields []DataFieldDefinition,
+) {
+	modules = []ModuleDefinition{
+		{ID: ModuleID, Version: ModuleVersion},
+	}
+	capabilities = []CapabilityDefinition{
+		{ModuleID: ModuleID, ID: CapabilityPassword, Version: CapabilityPasswordVersion},
+		{ModuleID: ModuleID, ID: CapabilitySession, Version: CapabilitySessionVersion},
+	}
+	dataFields = []DataFieldDefinition{
+		{
+			ModuleID: ModuleID, CapabilityID: CapabilityPassword, CapabilityVersion: CapabilityPasswordVersion,
+			ID: FieldEmail, Version: FieldEmailVersion, ChangeKind: ChangeAdditive,
+		},
+		{
+			ModuleID: ModuleID, CapabilityID: CapabilityPassword, CapabilityVersion: CapabilityPasswordVersion,
+			ID: FieldPasswordHash, Version: FieldPasswordHashVer, ChangeKind: ChangeIncompatible,
+		},
+	}
+	return modules, capabilities, dataFields
+}
